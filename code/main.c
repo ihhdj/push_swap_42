980e3ce501031c569e7bafa9385a7fd62850c5ee
Toony06 <tony.roman.pro@outlook.fr>
2025-01-17 15:00:05 +0100

    iheb le goat

--- code/main.c
+++ code/main.c
@@ -6,7 +6,7 @@
 /*   By: iheb <iheb@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 12:33:37 by ihhadjal          #+#    #+#             */
-/*   Updated: 2025/01/17 14:26:30 by iheb             ###   ########.fr       */
+/*   Updated: 2025/01/17 14:58:52 by iheb             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,18 @@ int main(int argc, char **argv)
 		return (ft_printf("Error: the argument is empty\n"), 1);
 	if (argc == 2)
 	{
+		i = 0;
 		argv_split = ft_split(argv[1], 32);
 		argv = argv_split;
+		while (argv[i])
+			parsing(argv[i++]);
 	}
-	while (argv[i])
-		parsing(argv[i++]);
-	free_argv(argv_split);
+	if (argc > 1)
+	{
+		while (argv[i])
+			parsing(argv[i++]);
+	}
+	if (argv_split)
+		free_argv(argv_split);
 	return (0);
 }
--- code/parsing.c
+++ code/parsing.c
@@ -6,7 +6,7 @@
 /*   By: iheb <iheb@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 16:00:30 by ihhadjal          #+#    #+#             */
-/*   Updated: 2025/01/17 14:11:01 by iheb             ###   ########.fr       */
+/*   Updated: 2025/01/17 14:58:04 by iheb             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int	check_string(char *str)
 	i = 0;
 	while (str[i])
 	{
-		if (!ft_isdigit(str[i]))
+		if (!ft_isdigit(str[i]) && str[i] != '-' && str[i] != '+')
 			return(ft_printf("Error: insert numbers only\n"), 1);
 		i++;
 	}
